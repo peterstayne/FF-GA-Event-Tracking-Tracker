a86a5b4fac28ede05ec8e5be2e2814f358e74b3f
Pete Milkman <pmilkman@gmail.com>
2022-12-30 23:56:46 -0500

    initial FF launch version

---
+++ background.js
@@ -0,0 +1,259 @@
+    function saveEvents(events) {
+        return chrome.storage.local.set({ 'gae': { eventlist: events }});
+    }
+
+    function addEvent(pushData) {
+        // console.log('adding event!', pushData);
+        chrome.storage.local.get('gae', (result) => { 
+
+            // console.log('result', result);
+            if(typeof result.gae == 'undefined') {
+                result.gae = { 'eventlist': [] };
+            }
+            if(typeof result.gae.eventlist === 'undefined') {
+                result.gae.eventlist = [];
+            }
+            if(result.gae.eventlist.length > 100) {
+                result.gae.eventlist.shift();
+            }
+            let today = new Date();
+            let hrs = today.getHours();
+            let ampm = 'am';
+            if(hrs > 12) {
+                hrs -= 12;
+                ampm = 'pm';
+            }
+            pushData.ts = hrs + ":" + today.getMinutes() + ":" + today.getSeconds() + ampm;
+            result.gae.eventlist.push(pushData); 
+            saveEvents(result.gae.eventlist); 
+        });
+    }
+
+    /**
+     * Credit
+     * http://stackoverflow.com/questions/901115/get-query-string-values-in-javascript/901144#901144
+     */
+    function getParameterByName( url, name ) {
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+
+        var regexS = "[\\?&]" + name + "=([^&#]*)";
+        var regex = new RegExp(regexS);
+        var results = regex.exec(url);
+
+        if ( results === null ) {
+            return '';
+        } else {
+            return decodeURIComponent(results[1].replace(/\+/g, " "));
+        }
+    }
+
+    // Perform the callback when a request is received from the content script
+    chrome.runtime.onMessage.addListener(function(request) 
+    { 
+        // Get the first callback in the callbacks array
+        // and remove it from the array
+        // console.log('orq', request);
+        var callback = callbacks.shift();
+
+        // Call the callback function
+        callback(request); 
+    }); 
+    chrome.webRequest.onBeforeSendHeaders.addListener(
+      function(details) {
+        var referer, eventString, uacode;
+        var category, action, label, val;
+        for (var i = 0; i < details.requestHeaders.length; ++i) {
+          if (details.requestHeaders[i].name === 'User-Agent') {
+            details.requestHeaders.splice(i, 1);
+            break;
+          }
+        }
+        if(details.url.indexOf('google-analytics.com/collect') > -1 && getParameterByName(details.url, 't').toLowerCase() === 'event') {
+
+            referer = tabid = eventString = uacode = category = action = label = val = '<i>null</i>';
+
+            tabid = details.tabId;
+            category = getParameterByName(details.url, 'ec');
+            action = getParameterByName(details.url, 'ea');
+            label = getParameterByName(details.url, 'el');
+            val = getParameterByName(details.url, 'ev');
+            uacode = getParameterByName(details.url, 'tid');
+            referer = getParameterByName(details.url, 'dl');
+
+            let ev = {
+                tabid: tabid,
+                uacode: uacode,
+                referer: referer,
+                data: {
+                    Category: category,
+                    Action: action,
+                    Label: label,
+                    Value: val
+                }
+            };
+            addEvent(ev);
+            // if(eventObject.length > 25) eventObject.shift();
+        }
+        if(details.url.indexOf('google-analytics.com/g/collect') > -1 && getParameterByName(details.url, '_ee') == 1 && getParameterByName(details.url, 'en') != 'page_view') {
+            // console.log('obsh', details.url, details)
+            referer = tabid = eventString = property = uacode = category = action = label = val = '<i>null</i>';
+
+            let ps = new URLSearchParams(details.url);
+            let evp = {};
+
+            for (const key of ps.keys()) {
+                if(key.substr(0,3) == 'ep.') {
+                    evp[key.substr(3)] = ps.get(key);
+                }
+                if(key.substr(0,3) == 'en') {
+                    evp['Category'] = ps.get(key);
+                }
+                if(key.substr(0,4) == 'epn.') {
+                    evp[key.substr(4)] = ps.get(key) + 0;
+                }
+            }
+            // console.log('evp', evp)
+
+            tabid = details.tabId;
+            category = getParameterByName(details.url, 'en');
+            // action = getParameterByName(details.url, 'ea');
+            // label = getParameterByName(details.url, 'el');
+            val = JSON.stringify(evp);
+            uacode = getParameterByName(details.url, 'tid');
+            property = getParameterByName(details.url, 'dt');
+            referer = getParameterByName(details.url, 'dl');
+
+            let ev = {
+                tabid: tabid,
+                uacode: uacode,
+                referer: referer,
+                data: evp
+            };
+
+            // console.log(ps.keys());
+            addEvent(ev);
+            // if(eventObject.length > 25) eventObject.shift();
+        }
+
+        if(details.url.indexOf('_utm') > -1) {
+
+            referer = eventString = uacode = category = action = label = val = '<i>null</i>';
+
+            for(var i in details.requestHeaders) {
+                if(details.requestHeaders[i].name == "Referer") {
+                    referer = details.requestHeaders[i].value;
+                }
+            }
+            eventString = getParameterByName(details.url, 'utme');
+
+            if(eventString.substr(0,1) === '5') {
+
+                if(eventString.indexOf(')8(') > -1) eventString = eventString.substring(0, eventString.indexOf(')8(') + 1);
+                if(eventString.indexOf(')9(') > -1) eventString = eventString.substring(0, eventString.indexOf(')9(') + 1);
+                if(eventString.indexOf(')11(') > -1) eventString = eventString.substring(0, eventString.indexOf(')11(') + 1);
+                eventString = eventString.substring(2, eventString.length - 1).split(/\*|\)\(/);
+                category = eventString[0];
+                action = eventString[1];
+                if(eventString.length > 2) {
+                    label = eventString[2]; 
+                    if(eventString.length > 3) {
+                        val = eventString[3]; 
+                    }
+                }
+                uacode = getParameterByName(details.url, 'utmac');
+                let ev = {
+                    tabid: tabid,
+                    uacode: uacode,
+                    referer: referer,
+                    data: {
+                        Category: category,
+                        Action: action,
+                        Label: label,
+                        Value: val
+                    }
+                };
+                addEvent(ev);
+                // if(eventObject.length > 15) eventObject.shift();
+                // console.log(eventObject);
+
+            }
+        }
+        return {requestHeaders: details.requestHeaders};
+      },
+      {urls: ["<all_urls>"]},
+      ["requestHeaders"]
+    );
+
+function createWindow() {
+    chrome.windows.create({
+        // Just use the full URL if you need to open an external page
+        url: chrome.runtime.getURL("popup.html"),
+        type: "popup",
+        width: 720,
+        height: 320
+    }, function(chromeWindow) {
+        chromeWindow.alwaysOnTop = true;
+        chrome.storage.local.set({ 'cw': chromeWindow });
+        // console.log('chromewindow', chromeWindow);
+    });
+}
+
+var cw = false;
+
+browser.browserAction.onClicked.addListener(function(tab) {
+    // console.log('i am clicked!!');
+    chrome.storage.local.get(['cw'], function (result) {
+        if(typeof result.cw !== 'undefined' && typeof result.cw.id != 'undefined') {
+            chrome.windows.get(result.cw.id, function(chromeWindow) {
+                // console.log('loop cw', chromeWindow);
+                if (!chrome.runtime.lastError && chromeWindow) {
+                    chrome.windows.update(result.cw.id, {focused: true});
+                    // console.log('vid found!');
+                    return true;
+                }
+                createWindow();
+            });
+        } else {
+            createWindow();
+        }
+    });
+});
+
+/*
+
+https://www.google-analytics.com/g/collect?
+v=2
+&
+tid=G-MG2R3W5D76
+&
+gtm=2oebu0
+&
+_p=1087094531
+&
+cid=1641160515.1670773322
+&
+ul=en-us
+&
+sr=1920x1080
+&
+_s=3
+&
+sid=1670960899
+&
+sct=6
+&
+seg=1
+&
+dl=http%3A%2F%2Fpetemilkman.com%2F
+&
+dt=pete%20milkman%20.%20com
+&
+en=topmenu
+&
+_ee=1
+&
+ep.label=experiments
+&
+_et=10487
+
+*/
---
+++ popup.js
@@ -0,0 +1,293 @@
+        // This callback function is called when the content script has been 
+        // injected and returned its results
+        function onPageInfo(o) 
+        { 
+        } 
+
+        // POST the data to the server using XMLHttpRequest
+        function addBookmark(f)
+        {
+            return false;
+        }
+
+        function createCSV(bgeo, tabfilter) {
+           if(bgeo.length) {
+                let countUntil;
+                if(bgeo.length > 100) {
+                    countUntil = bgeo.length - 100;
+                } else {
+                    countUntil = -1;
+                }
+                let cols = {};
+                for(var i = bgeo.length-1; i > countUntil; i--) {
+                    if(typeof bgeo[i].data === 'undefined') continue;
+                    if(tabfilter != 'all' && bgeo[i].tabid != tabfilter) continue;
+                    Object.keys(bgeo[i].data).forEach(function(k) {
+                        if(k != 'Category') cols[k] = true;
+                    });
+                }
+                let colnames = Object.keys(cols);
+                let csv = 'URL,UA String,Time,Category';
+                colnames.forEach(function(cname) {
+                    csv+= ',"' + cname + '"';
+                });
+                csv+= "\n";
+
+                for(let i = bgeo.length-1; i > countUntil; i--) {
+                     if(typeof bgeo[i].data === 'undefined') continue;
+                    if(tabfilter != 'all' && bgeo[i].tabid != tabfilter) continue;
+                   csv += bgeo[i].referer + ",";
+                    csv += bgeo[i].uacode + ",";
+                    csv += bgeo[i].ts + ",";
+                    csv += '"' + bgeo[i].data.Category + '"';
+
+                    colnames.forEach(function(cname) {
+                        if(bgeo[i].data[cname]) {
+                            csv += ',"' + bgeo[i].data[cname] + '"';
+                        } else {
+                            csv += ',';
+                        }
+                    });
+                    csv += "\n";
+                }
+                let he = document.createElement('a');  
+                he.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);  
+                he.target = '_blank';  
+                he.download = 'EventTrackingTracker.csv';  
+                he.click();  
+           }
+        }
+
+        function updateTable(bgeo, tabfilter) {
+            var newhtml = '';
+            var dispURL = '';
+            var numRows = 0;
+            if(bgeo.length) {
+                var countUntil;
+                if(bgeo.length > 100) {
+                    countUntil = bgeo.length - 100;
+                } else {
+                    countUntil = -1;
+                }
+                let cols = {};
+                for(var i = bgeo.length-1; i > countUntil; i--) {
+                    if(typeof bgeo[i].data === 'undefined') continue;
+                    if(tabfilter != 'all' && bgeo[i].tabid != tabfilter) continue;
+                    numRows+=1;
+                    Object.keys(bgeo[i].data).forEach(function(k) {
+                        if(k != 'Category') cols[k] = true;
+                    });
+                }                
+                let colnames = Object.keys(cols);
+                // console.log(colnames, cols);
+                let evhead = '<tr>';
+                    evhead+= '<th><a href="#" id="expander">URL <span class="harr">&harr;</span></a></th>';
+                    evhead+= '<th>UA String</th>';
+                    evhead+= '<th>Time</th>';
+                    evhead+= '<th>Category</th>';
+                    colnames.forEach(function(cname) {
+                        evhead+= '<th>' + cname + '</th>';
+                    });
+                    evhead+= '</tr>';
+                document.getElementById('event-list-head').innerHTML = evhead;
+
+                for(var i = bgeo.length-1; i > countUntil; i--) {
+                    if(typeof bgeo[i].data === 'undefined') continue;
+                    if(tabfilter != 'all' && bgeo[i].tabid != tabfilter) continue;
+                    if(bgeo[i].referer.length > 37) {
+                        dispURL = bgeo[i].referer.substr(0, 32) + ' ...';
+                    } else {
+                        dispURL = bgeo[i].referer;
+                    }
+                    newhtml += '<tr class="entry">';
+                        newhtml += '<td class="url">';
+                            newhtml += '<span class="compact">' + dispURL + '</span>';
+                            newhtml += '<span class="full">' + bgeo[i].referer + '</span>';
+                        newhtml += '</td>';
+                        newhtml += '<td class="uastring">' + bgeo[i].uacode + '</td>';
+                        newhtml += '<td class="uastring">' + bgeo[i].ts + '</td>';
+                        newhtml += '<td class="category">' + bgeo[i].data.Category + '</td>';
+                        colnames.forEach(function(cname) {
+                            if(bgeo[i].data[cname]) {
+                                newhtml += '<td>' + bgeo[i].data[cname] + '</td>';
+                            } else {
+                                newhtml += '<td>&nbsp;</td>';
+                            }
+                        });
+                    newhtml += '</tr>';
+                }
+            }
+            if(!numRows) {
+                newhtml = '<tr><td colspan="100%" align="center" class="no-results">';
+                newhtml += '<p><i>No events recorded yet.</i></p>';
+                newhtml += '<p><i><a href="https://support.google.com/analytics/answer/9322688?hl=en&ref_topic=9756175" target="_blank">Google\'s Event documentation page</a></i></p>';
+                newhtml += '<p><i>Note: Some ad blockers like uBlock Origin block events from firing.</i></p>';
+                newhtml += '</td></tr>';
+            }
+            document.getElementById('event-list').innerHTML = newhtml;            
+        }
+
+        async function updateList(target = "web") {
+            let bgeo = [];
+            let tabfilter = 'all-tabs';
+            let result = await readLocalStorage('gae');
+            let prefs_tab = await readLocalStorage('prefs_tab');
+            let tabids = [];
+            if(typeof result != 'undefined' && result && typeof result.eventlist != 'undefined') {
+                bgeo = result.eventlist;
+                bgeo.forEach(function(ev) {
+                    if(ev.tabid) {
+                        tabids.push(ev.tabid);
+                    }
+                });
+            }
+            if(typeof prefs_tab != 'undefined') {
+                tabfilter = prefs_tab;
+            }
+            chrome.tabs.query({}, function(tabs) {
+                // console.log('tabs', tabs);
+                var tabhtml = '<a href="#" class="tab ';
+                var found = false;
+                if(tabfilter == 'all-tabs') {
+                    tabhtml += 'active';
+                    found = true;
+                }
+                tabhtml += ' all-tabs" id="all-tabs">All Tabs</a>';
+                tabs.forEach(function(i) {
+                                    // console.log('tabids', i.tabid, tabids, i);
+
+                    if(!i.active && tabids.indexOf(i.id) === -1) {
+                        return;
+                    }
+                    // console.log(i);
+                    if(typeof i.url != 'undefined' && i.url && i.url.substr(0,10) != 'chrome-ext') {
+                        let title = i.title;
+                        if(title.length > 20) title = title.substr(0,18) + '...'
+                        tabhtml += '<a href="#" class="tab ';
+                        if(tabfilter == i.id) {
+                            tabhtml += 'active ';
+                            found = true;
+                        }
+                        let fi = i.favIconUrl;
+                        if(!fi) {
+                            fi = 'NoImage.png'
+                        }
+                        tabhtml += 'one-tab" id="tab' + i.id + '" data-tabid="' + i.id + '"><img src="' + fi + '" width="16" height="16" /> ' + title + '</a>';
+                    }
+                });
+                document.getElementById('tabs-container').innerHTML = tabhtml;
+                if(!found) {
+                    chrome.storage.local.set({ 'prefs_tab': 'all-tabs' });
+                }
+            });
+            if(tabfilter == 'all-tabs') {
+                if(target == 'web') {
+                    updateTable(bgeo, 'all');
+                } else if(target == 'csv') {
+                    createCSV(bgeo, 'all');
+                }
+            } else {
+                if(target == 'web') {
+                    updateTable(bgeo, tabfilter);
+                } else if(target == 'csv') {
+                    createCSV(bgeo, tabfilter);
+                }
+            }
+        }
+
+        function switchTab(tabid) {
+            document.querySelectorAll('.tab').forEach(function(i,a) {
+                if(i.id != tabid) {
+                    i.classList.remove('active');
+                } else {
+                    i.classList.add('active');
+                }
+            });
+        }
+
+        // Call the getPageInfo function in the background page, passing in 
+        // our onPageInfo function as the callback
+        window.onload = function() { 
+            document.getElementById('event-table').onclick = function(e) {
+                for (var target=e.target; target && target!=this; target=target.parentNode) {
+                    if(target.matches('#expander')) {
+                        var table = document.getElementById('event-table');
+                        var tableClasses = table.classList;
+                        if(tableClasses.contains('wrapit')) {
+                            tableClasses.remove('wrapit');
+                            chrome.storage.local.set({ 'prefs_expander': 'unexpanded' });
+                        } else {
+                            tableClasses.add('wrapit');
+                            chrome.storage.local.set({ 'prefs_expander': 'expanded' });
+                        }
+                        updateList();
+                    }
+                }
+            };
+            updateList();
+            setInterval(updateList, 3500);
+            document.getElementById('reset-list').onclick = function() {
+                chrome.storage.local.set({ 'gae': { eventlist: [] }});
+                updateList();
+            };
+            document.getElementById('tabs-container').onclick = function(e) {
+                // console.log(e.target);
+                for (var target=e.target; target && target!=this; target=target.parentNode) {
+                    if(target.matches('.tab')) {
+                        switchTab(target.id);
+                        break;
+                    }
+                }
+                if(target.id == 'all-tabs') {
+                    chrome.storage.local.set({ 'prefs_tab': 'all-tabs' });
+                } else {
+                    chrome.storage.local.set({ 'prefs_tab': target.dataset.tabid });
+                }
+                updateList();
+            };
+            document.getElementById('export-list').onclick = function() {
+                updateList('csv');
+            };
+            chrome.storage.local.get(['prefs_tab'], function (result) {
+                if(typeof result.prefs_tab != 'undefined') {
+                    switchTab(result.prefs_tab);
+                } else {
+                    chrome.storage.local.set({ 'prefs_tab': 'all-tabs' });
+                }
+            });
+            chrome.storage.local.get(['prefs_expander'], function (result) {
+                if(typeof result.prefs_expander != 'undefined') {
+                    if(result.prefs_expander == 'expanded') {
+                        document.getElementById('event-table').classList.add('wrapit');
+                    }
+                }
+            });
+            chrome.storage.local.get(['prefs_width', 'prefs_height'], function (result) {
+                if(typeof result.prefs_width != 'undefined' && typeof result.prefs_height != 'undefined') {
+                    window.resizeTo(result.prefs_width, result.prefs_height);
+                }
+            });            
+            // document.getElementById('alwaysontop').onchange = function() {
+            //     var alwaysOnTop;
+            //     if(this.checked) { 
+            //         alwaysOnTop = false;
+            //     } else {
+            //         alwaysOnTop = true;
+            //     }
+            //     chrome.storage.local.get(['cw'], function (result) {
+            //         console.log(result);
+            //         result.cw.alwaysOnTop = alwaysOnTop;
+            //     });
+            // };
+        };
+        window.onresize = function() {
+            chrome.storage.local.set({ 'prefs_width': window.innerWidth });
+            chrome.storage.local.set({ 'prefs_height': window.innerHeight });
+        }
+  const readLocalStorage = async (key) => {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get([key], function (result) {
+          resolve(result[key]);
+      });
+    });
+  };
